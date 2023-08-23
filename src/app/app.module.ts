e99c1515ae87a77a6e43ab7eba6a8085367b9a1c
abdulwahidkhoeruddin <abdulwahidkhoeruddin@gmail.com>
2023-08-24 01:51:34 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatChipsModule} from "@angular/material/chips";
+import { LoginComponent } from './share/component/login/login.component';
+import { RegisterComponent } from './share/component/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+    MatChipsModule
   ],
+  // standalone: true,
   providers: [],
   bootstrap: [AppComponent]
 })
