bd7e43c2f7098c7960b9fbd51b704225f92aea59
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-08-24 12:06:27 +0700

    Routing first set up

---
+++ src/app/pages-admin/pages-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PagesAdminRoutingModule} from "./pages-admin-routing.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PagesAdminRoutingModule
+  ]
+})
+export class PagesAdminModule { }
