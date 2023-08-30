89abb0546ca4d506eb0a81116250a5f4cd058700
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-08-30 14:28:06 +0700

    submission response

---
+++ src/app/share/model/response/submission-response.model.ts
@@ -0,0 +1,12 @@
+export interface SubmissionResponse {
+    id: string
+    debtorId: string,
+    umkmId: string,
+    umkmName: string,
+    date: Date,
+    loanAmount: number,
+    tenor: number,
+    debt: number,
+    monthlyDebt: number,
+    isApprove: boolean
+}
