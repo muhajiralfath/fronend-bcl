cef8b6fd2c784ee7fe77049587986cb2563c631a
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-30 11:01:45 +0700

    [Feature] finish debt submission

--- src/app/pages-debtor/submission/submission.component.ts
+++ src/app/pages-debtor/submission/submission.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonResponse} from "../../share/model/response/common-response.model";
+import {DebtorResponse} from "../../share/model/response/debtor-response.model";
+import {DebtorService} from "../../share/service/debtor/debtor.service";
+import {SubmissionService} from "../../share/service/submission/submission.service";
+import {Router} from "@angular/router";
+import Swal from "sweetalert2";
+import {SubmissionResponse} from "../../share/model/response/submission-response.model";
+
 
 @Component({
   selector: 'app-submission',
@@ -7,4 +15,38 @@ import { Component } from '@angular/core';
 })
 export class SubmissionComponent {
 
+  debtorId: string = ""
+  submissionList: SubmissionResponse[] = []
+
+  constructor(private readonly debtorService: DebtorService,
+              private readonly submissionService: SubmissionService,
+              private readonly router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.getDebtorId()
+  }
+
+  getSubmissionByDebtorId():void {
+    this.submissionService.getSubmissionByDebtorId(this.debtorId).subscribe({
+      next: (result) => {
+        this.submissionList = result.data
+      }
+    })
+  }
+
+  getDebtorId(): void {
+    this.debtorService.getByToken().subscribe({
+      next: (debtorResponse: CommonResponse<DebtorResponse>) => {
+        let data: DebtorResponse = debtorResponse.data;
+        this.debtorId = data.debtorId
+        console.log(this.debtorId)
+        this.getSubmissionByDebtorId()
+      },
+      error: () => {
+        Swal.fire("Don't have submission , add firt")
+        this.router.navigateByUrl("/debtor/submissions")
+      }
+    })
+  }
 }
