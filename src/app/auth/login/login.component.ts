50994693114d0acaa53519a018f4ee8deae6a6c8
akbarabaayy01 <akbarabaayy01@gmail.com>
2023-09-19 23:24:22 +0700

    email registrasi langsung tersedia  di login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,45 +1,49 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import Swal from "sweetalert2";
-import {AuthRequest} from "../../share/model/request/auth-request.model";
-import {AuthService} from "../../share/service/auth/auth.service";
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { AuthRequest } from '../../share/model/request/auth-request.model';
+import { AuthService } from '../../share/service/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   constructor(
-    private readonly service:AuthService,
-    private readonly router:Router
+    private readonly service: AuthService,
+    private readonly router: Router
   ) {}
 
-  form:FormGroup = new FormGroup({
-    email: new FormControl("", Validators.required),
-    password: new FormControl("", Validators.required)
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
 
-  ngOnInit(): void{
-    const token = sessionStorage.getItem("token");
+  ngOnInit(): void {
+    const token = sessionStorage.getItem('token');
+    const registeredEmail = sessionStorage.getItem('registeredEmail');
     if (token) {
-      this.router.navigateByUrl("/debtor");
+      this.router.navigateByUrl('/debtor');
+    } else if (registeredEmail) {
+      this.form.patchValue({ email: registeredEmail });
+      sessionStorage.removeItem('registeredEmail');
     }
   }
 
-  save(data:AuthRequest){
+  save(data: AuthRequest) {
     this.service.login(data).subscribe({
-      next:(res) => {
-        let token = res.data.token
-        if(token){
-          sessionStorage.setItem("token", token);
-          this.router.navigateByUrl("/debtor/home");
+      next: (res) => {
+        let token = res.data.token;
+        if (token) {
+          sessionStorage.setItem('token', token);
+          this.router.navigateByUrl('/debtor/home');
         }
       },
       error: (err) => {
-        Swal.fire("Invalid Username or Password!");
-      }
+        Swal.fire('Invalid Username or Password!');
+      },
     });
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,46 +1,51 @@
 import { Component } from '@angular/core';
-import {AuthService} from "../../share/service/auth/auth.service";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import Swal from "sweetalert2";
-import {AuthRequest} from "../../share/model/request/auth-request.model";
-import {Router} from "@angular/router";
+import { AuthService } from '../../share/service/auth/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { AuthRequest } from '../../share/model/request/auth-request.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   constructor(
-    private readonly service:AuthService,
+    private readonly service: AuthService,
     private readonly router: Router
   ) {}
-  form:FormGroup = new FormGroup({
-    email: new FormControl("", Validators.required),
-    password: new FormControl("", Validators.required),
-    repeatPassword: new FormControl("", Validators.required)
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    repeatPassword: new FormControl('', Validators.required),
   });
 
-  saveRegis(request:any){
-    if(request.password !== request.repeatPassword){
+  saveRegis(request: any) {
+    if (request.password !== request.repeatPassword) {
       this.form.reset();
-      Swal.fire(
-        "Login", "Passwords are not the same","error"
-      );
-    }else{
-      let authRequest:AuthRequest = {
+      Swal.fire('Login', 'Passwords are not the same', 'error');
+    } else {
+      let authRequest: AuthRequest = {
         email: this.form.get('email')?.value,
-        password: this.form.get("password")?.value
+        password: this.form.get('password')?.value,
       };
       this.service.registerDebtor(authRequest).subscribe({
         next: (result) => {
+          sessionStorage.setItem('registeredEmail', authRequest.email);
+
           this.form.reset();
-          Swal.fire("Registration successful!", "You can login!", "success");
-          this.router.navigateByUrl("/login")
-        }, error:(e) => {
+          Swal.fire('Registration successful!', 'You can login!', 'success');
+          this.router.navigateByUrl('/login');
+        },
+        error: (e) => {
           this.form.reset();
-          Swal.fire("Register Failed", "Email already exist, Please use another one!", "error");
-        }
+          Swal.fire(
+            'Register Failed',
+            'Email already exist, Please use another one!',
+            'error'
+          );
+        },
       });
     }
   }
